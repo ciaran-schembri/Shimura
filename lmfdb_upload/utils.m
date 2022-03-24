9a7b9141db86f3b644c38c86ae051ac04faf9e90
Sam Schiavone <sam.schiavone@gmail.com>
2022-03-24 18:59:59 -0400

    replace string

--- lmfdb_upload/utils.m
+++ lmfdb_upload/utils.m
@@ -20,6 +20,44 @@ intrinsic sprint(X::.) -> MonStgElt
     return strip(Sprintf("%o",X));
 end intrinsic;
 
+intrinsic ReplaceString(s::MonStgElt, fs::MonStgElt, ts::MonStgElt) -> MonStgElt
+  {Return a string obtained from the string s by replacing all occurences of fs with ts.}
+  i:=Position(s,fs);
+  if i eq 0 then
+    strg:=s;  // nothing to find
+  elif (i+#fs-1) eq #s   then // if fs is at end
+    strg:=Substring(s,1,i-1) cat ts;
+  elif i eq 1 then // if fs is at beginning
+    strg:=ts cat $$(Substring(s,i+#fs,#s-i),fs,ts);
+  else
+    strg:=Substring(s,1,i-1) cat ts cat $$(Substring(s,i+#fs,#s-i),fs,ts); // recursively call on tail
+  end if;
+  return strg;
+end intrinsic;
+
+intrinsic ReplaceString(s::MonStgElt, fs::[MonStgElt], ts::[MonStgElt]) -> MonStgElt
+  {Return a string obtained from the string s by replacing all occurences of strings in fs with strings in ts.}
+  //assert fs ne ts;
+  for i:=1 to #fs do
+    s:=ReplaceString(s,fs[i],ts[i]);
+  end for;
+  return s;
+end intrinsic;
+
+// procedure versions
+intrinsic ReplaceString(~s::MonStgElt, fs::MonStgElt, ts::MonStgElt)
+  {In the string s, replace all occurences of fs with ts.}
+  s := ReplaceString(s,fs,ts);
+end intrinsic;
+
+intrinsic ReplaceString(~s::MonStgElt, fs::[MonStgElt], ts::[MonStgElt])
+  {In the string s, replace all occurences of strings in fs with strings in ts.}
+  for i:=1 to #fs do
+    ReplaceString(~s,fs[i],ts[i]);
+  end for;
+end intrinsic;
+
+
 // functions for writing ShimuraDB properties to text file
 //
 
